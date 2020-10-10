672ad7c59ee0796aba42c082eef37c5ffd46eb3e
Fellsing <70785853+Fellsing@users.noreply.github.com>
2020-10-10 19:02:05 +0600

    Update app.js

--- app.js
+++ app.js
@@ -59,10 +59,12 @@ var __generator = (this && this.__generator) || function (thisArg, body) {
 //     let a = document.createElement("img");
 //     a.src = 'back.jpg';
 //     //@ts-ignore 
+//     //start of task d
 //     await new Promise( (resolve, reject)=>{
 //         setTimeout(resolve, 12000);
 //         a.onerror = () => reject(new Error("error while loading"));
 //     });
+//     //end of task d
 //     document.body.appendChild(a);
 //     return a;
 // }
@@ -70,20 +72,6 @@ var __generator = (this && this.__generator) || function (thisArg, body) {
 // promise.then(success1, error1);
 // promise.then(success1).then(success1).catch(error1);
 // promise.finally(()=>console.log("done"));
-//task d
-// function loadImage(success,error){
-//     let a = document.createElement("img");
-//     a.src = 'back.jpg';
-//     document.body.appendChild(a);
-//     a.onload = success;
-//     a.onerror = error;
-// }
-// loadImage(() => {
-//     console.log('image loaded')},
-//      ()=>{
-//         console.log('load failed');
-//     });
-// ;
 //task e
 function success1() {
     console.log("Loaded");
@@ -101,8 +89,8 @@ function loadImage() {
                     b = document.createElement("img");
                     c = document.createElement("img");
                     a.src = 'back.jpg';
-                    b.src = '007gNgEOly1gihvnjucqlj31sg1sghdt.jpg';
-                    c.src = '118385103_114625810361587_8571102593084231161_n.jpg';
+                    b.src = 'back.jpg';
+                    c.src = 'back.jpg';
                     array = [a, b, c];
                     //@ts-ignore 
                     return [4 /*yield*/, new Promise(function (resolve, reject) {
@@ -126,3 +114,4 @@ var promise = loadImage();
 promise.then(success1, error1);
 //promise.then(success1).then(success1).catch(error1);
 promise["finally"](function () { return console.log("done"); });
+
